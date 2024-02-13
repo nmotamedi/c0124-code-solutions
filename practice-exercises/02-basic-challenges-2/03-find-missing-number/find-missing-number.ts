eb82aabf6dd2c211e5409efa10d6632fdda3c5e9
“Nader <nadmot23@gmail.com>
2024-02-13 01:44:21 +0000

    Additional exercise for 02.

--- practice-exercises/02-basic-challenges-2/03-find-missing-number/find-missing-number.ts
+++ practice-exercises/02-basic-challenges-2/03-find-missing-number/find-missing-number.ts
@@ -1,3 +1,8 @@
 export function findMissingNumber(arr: number[]): number | undefined {
-  return undefined;
+  const fullSum = ((arr.length + 1) * (arr.length + 1 + 1)) / 2;
+  let sum = 0;
+  for (const number of arr) {
+    sum += number;
+  }
+  return fullSum - sum;
 }
