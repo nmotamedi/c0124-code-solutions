395aa340c2bdcab7d9044abbd3ba8dadaa78680c
“Nader <nadmot23@gmail.com>
2024-02-13 00:24:49 +0000

    Added 2 exercises to 02

--- practice-exercises/02-basic-challenges-2/02-display-likes/display-likes.ts
+++ practice-exercises/02-basic-challenges-2/02-display-likes/display-likes.ts
@@ -1,3 +1,15 @@
 export function displayLikes(names: string[]): string {
-  return '';
+  const listLength = names.length;
+  switch (listLength) {
+    case 0:
+      return 'no one likes this';
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${listLength - 2} others like this`;
+  }
 }
