c427c8f3f123df7bf7e1233b0795414801051b55
“Nader <nadmot23@gmail.com>
2024-02-14 22:30:18 +0000

    Added 05-valid-anagrams

--- practice-exercises/03-high-order-array-methods/05-valid-anagrams/valid-anagrams.ts
+++ practice-exercises/03-high-order-array-methods/05-valid-anagrams/valid-anagrams.ts
@@ -1,6 +1,12 @@
-/**
- * Checks if two strings are valid anagrams of each other.
- */
+// Not equal to what the given solution is
 export function validAnagrams(str1: string, str2: string): boolean {
-  return false;
+  const arr1: string[] = str1.split('');
+  const arr2: string[] = str2.split('');
+  const str1sort = arr1.sort().join();
+  const str2sort = arr2.sort().join();
+  if (str1sort === str2sort) {
+    return true;
+  } else {
+    return false;
+  }
 }
