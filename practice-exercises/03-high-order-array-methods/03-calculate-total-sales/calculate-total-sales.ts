a871d1f730b99dc6b0465caa70b35b466e5fc053
“Nader <nadmot23@gmail.com>
2024-02-14 17:08:04 +0000

    Added exercises 2, 3, 4 in high-order-array-methods

--- practice-exercises/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.ts
+++ practice-exercises/03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.ts
@@ -8,5 +8,8 @@ export function calculateTotalSalesWithTax(
   products: Product[],
   taxRate: number
 ): number {
-  return NaN;
+  const total = products.reduce((itemSum: number, product: Product) => {
+    return itemSum + product.price * product.quantity;
+  }, 0);
+  return total + total * (taxRate / 100);
 }
