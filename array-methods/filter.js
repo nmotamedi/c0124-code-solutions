6f3726ee24392e2e5946b861383e04b3d605af30
“Nader <nadmot23@gmail.com>
2024-02-27 23:19:27 +0000

    updated js

--- array-methods/filter.js
+++ array-methods/filter.js
@@ -1,19 +1,17 @@
-'use strict';
+"use strict";
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const names = [
-  'Ada',
-  'Hedy',
-  'Jean',
-  'Grace',
-  'Evelyn',
-  'Joan',
-  'Elizabeth',
-  'Janese',
-  'Donna',
+    'Ada',
+    'Hedy',
+    'Jean',
+    'Grace',
+    'Evelyn',
+    'Joan',
+    'Elizabeth',
+    'Janese',
+    'Donna',
 ];
 const evens = numbers.filter((num) => num % 2 === 0);
 console.log('even numbers', evens);
-const filteredNames = names.filter(
-  (name) => !name.includes('d') && !name.includes('D')
-);
+const filteredNames = names.filter((name) => !name.includes('d') && !name.includes('D'));
 console.log('No D', filteredNames);
--- array-methods/map.js
+++ array-methods/map.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 const prices = [42.42, 10, 28.2234, 3.2, 5, 12];
 const pricesArray = prices.map((priceParam) => {
-  return {
-    price: priceParam,
-    salePrice: priceParam / 2,
-  };
+    return {
+        price: priceParam,
+        salePrice: priceParam / 2,
+    };
 });
 console.log('Price Objects', pricesArray);
 const formattedPrices = prices.map((priceParam) => {
-  return `$${priceParam.toFixed(2)}`;
+    return `$${priceParam.toFixed(2)}`;
 });
 console.log('Formatted prices: ', formattedPrices);
