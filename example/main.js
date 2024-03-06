a42842cf09151c54dbd4d7f9817826168c31f519
“Nader <nadmot23@gmail.com>
2024-03-06 01:28:30 +0000

    Updated testing for NPS API

--- example/main.js
+++ example/main.js
@@ -1,15 +1,23 @@
 "use strict";
-const url = 'https://cartes.io/api/maps';
+const url = 'https://developer.nps.gov/api/v1/parks?limit=500';
 const headers = {
-    "Accept": "application/json",
+    'X-Api-Key': 'etA2FldfC7HhYmqU7qvOsi5HIeCezAaSefRG26Hk',
 };
 async function getParkData() {
     try {
         const resp = await fetch(url, { headers: headers });
         if (!resp.ok)
             throw new Error("Network Error");
-        const YoseThingsData = await resp.json();
-        console.log(YoseThingsData);
+        const allParkData = await resp.json();
+        const npParkData = allParkData.data.filter((park) => {
+            return (park.designation === 'National Park' ||
+                park.designation === 'National Park & Preserve' ||
+                park.designation === 'National Parks' ||
+                park.designation === 'National and State Parks' ||
+                park.parkCode === "npsa");
+        });
+        const npParkNames = npParkData.map((park) => park.fullName);
+        console.log(npParkData);
     }
     catch (e) {
         console.error(e);
--- example/main.ts
+++ example/main.ts
@@ -4,17 +4,27 @@ interface Park {
   parkCode: string,
 }
 
-const url = 'https://cartes.io/api/maps';
+const url = 'https://developer.nps.gov/api/v1/parks?limit=500';
 const headers = {
-  "Accept": "application/json",
+  'X-Api-Key': 'etA2FldfC7HhYmqU7qvOsi5HIeCezAaSefRG26Hk',
 };
 
 async function getParkData (): Promise<void> {
   try {
     const resp = await fetch(url, {headers: headers});
     if (!resp.ok) throw new Error("Network Error");
-    const YoseThingsData = await resp.json();
-    console.log(YoseThingsData);
+    const allParkData = await resp.json();
+    const npParkData = allParkData.data.filter((park: Park) => {
+      return (
+        park.designation === 'National Park' ||
+        park.designation === 'National Park & Preserve' ||
+        park.designation === 'National Parks' ||
+        park.designation === 'National and State Parks' ||
+        park.parkCode === "npsa"
+      );
+    });
+    const npParkNames = npParkData.map((park: Park) => park.fullName);
+    console.log(npParkData);
   }
   catch (e) {
     console.error(e);
