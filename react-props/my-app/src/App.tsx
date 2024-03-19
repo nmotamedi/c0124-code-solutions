aa4dc745f533704e5d9a88c736c3dd9373d64bc5
“Nader <nadmot23@gmail.com>
2024-03-19 17:22:03 +0000

    Updated type inference and imageSRC variable name.

--- react-props/my-app/src/App.tsx
+++ react-props/my-app/src/App.tsx
@@ -5,18 +5,18 @@ import { ImageDescription } from './ImageDescription';
 import { ButtonContainer } from './ButtonContainer';
 import './App.css';
 
-const headerText: string = 'React Image Bank';
-const imgSRC: string = '/starry-sky.jpeg';
-const imgText: string = 'A beautiful Image of Space';
-const imgDescription: string =
+const headerText = 'React Image Bank';
+const imgSrc = '/starry-sky.jpeg';
+const imgText = 'A beautiful Image of Space';
+const imgDescription =
   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam incidunt sit dolorum accusantium sapiente reiciendis maxime dolores ullam delectus, commodi placeat eveniet, quam voluptates facere et magnam architecto quaerat velit voluptas rerum quos asperiores quis in';
-const buttonText: string = 'Click For Next Image';
+const buttonText = 'Click For Next Image';
 
 function App() {
   return (
     <>
       <Header text={headerText} />
-      <ImageContainer src={imgSRC} />
+      <ImageContainer src={imgSrc} />
       <ImageCaption text={imgText} />
       <ImageDescription text={imgDescription} />
       <ButtonContainer text={buttonText} />
