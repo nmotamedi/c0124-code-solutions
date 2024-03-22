3c35b8ae23e5d7beb0740e6b71511870bc2ff78f
“Nader <nadmot23@gmail.com>
2024-03-22 22:50:08 +0000

    react-multiple-components - Phase 2 - Nader Motamedi

--- react-multiple-components/my-app/src/RotatingBanner.tsx
+++ react-multiple-components/my-app/src/RotatingBanner.tsx
@@ -1,21 +1,24 @@
 import { Text } from './Text';
 import { PrevNextButtons } from './PrevNextButtons';
 import { Numbers } from './Numbers';
+import { useState } from 'react';
 
 type Prop = {
   items: string[];
 };
 
 export function RotatingBanner({ items }: Prop) {
+  const [currentIndex /*, setCurrentIndex */] = useState(2);
+
   return (
     <>
       <div>
-        <Text text={items[0]} />
+        <Text text={items[currentIndex]} />
       </div>
       <div>
         <PrevNextButtons text="Prev" />
       </div>
-      <Numbers length={items.length} index={0} />
+      <Numbers length={items.length} index={currentIndex} />
       <div>
         <PrevNextButtons text="Next" />
       </div>
