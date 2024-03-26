f8fc36e386aaac379a97127b5df298089209b0da
“Nader <nadmot23@gmail.com>
2024-03-26 22:01:16 +0000

    Complete typescript-closures - Nader

---
+++ typescript-closures/my-app/src/Counter.tsx
@@ -0,0 +1,17 @@
+import { useCallback, useEffect, useState } from 'react';
+
+export function Counter() {
+  const getData = useCallback(() => {
+    // fetch data
+    console.log('running');
+    return { foo: 'bar' };
+  }, []);
+
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    getData();
+    setCounter((prev) => prev + 1);
+  }, [getData]);
+
+  return <div>Fetching {counter} times</div>;
+}
