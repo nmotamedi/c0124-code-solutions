7be38a6cc4d7b737190d40eaadfafb3559490c3c
“Nader <nadmot23@gmail.com>
2024-03-27 15:54:44 +0000

    Updated file structure, moved component logic to CarouselTSX

--- react-carousel/my-app/src/App.tsx
+++ react-carousel/my-app/src/App.tsx
@@ -1,8 +1,5 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import { Buttons } from './Buttons';
 import { Carousel } from './Carousel';
-import { PrevNextButton } from './PrevNextbutton';
 
 const images = [
   {
@@ -32,39 +29,7 @@ const images = [
 ];
 
 function App() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const timeoutID = setTimeout(() => {
-      setCurrentIndex((currentIndex + 1) % images.length);
-    }, 3000);
-    return () => clearTimeout(timeoutID);
-  }, [currentIndex]);
-
-  return (
-    <>
-      <div className="row">
-        <PrevNextButton
-          onClick={() =>
-            setCurrentIndex((currentIndex - 1 + images.length) % images.length)
-          }
-          isLeft={true}
-        />
-        <Carousel image={images[currentIndex]} />
-        <PrevNextButton
-          onClick={() => setCurrentIndex((currentIndex + 1) % images.length)}
-          isLeft={false}
-        />
-      </div>
-      <div className="row">
-        <Buttons
-          onClick={(i: number) => setCurrentIndex(i)}
-          num={images.length}
-          index={currentIndex}
-        />
-      </div>
-    </>
-  );
+  return <Carousel images={images} />;
 }
 
 export default App;
--- react-carousel/my-app/src/Carousel.tsx
+++ react-carousel/my-app/src/Carousel.tsx
@@ -1,10 +1,47 @@
+import { useEffect, useState } from 'react';
+import { Buttons } from './Buttons';
+import { PrevNextButton } from './PrevNextbutton';
+import { Image } from './image';
+
 type Prop = {
-  image: {
+  images: {
     src: string;
     alt: string;
-  };
+  }[];
 };
 
-export function Carousel({ image }: Prop) {
-  return <img src={image.src} alt={image.alt} />;
+export function Carousel({ images }: Prop) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      setCurrentIndex((currentIndex + 1) % images.length);
+    }, 3000);
+    return () => clearTimeout(timeoutID);
+  }, [currentIndex, images.length]);
+
+  return (
+    <>
+      <div className="row">
+        <PrevNextButton
+          onClick={() =>
+            setCurrentIndex((currentIndex - 1 + images.length) % images.length)
+          }
+          isLeft={true}
+        />
+        <Image image={images[currentIndex]} />
+        <PrevNextButton
+          onClick={() => setCurrentIndex((currentIndex + 1) % images.length)}
+          isLeft={false}
+        />
+      </div>
+      <div className="row">
+        <Buttons
+          onClick={(i: number) => setCurrentIndex(i)}
+          num={images.length}
+          index={currentIndex}
+        />
+      </div>
+    </>
+  );
 }
