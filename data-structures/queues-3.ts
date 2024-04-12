0d6a9276fb1313e664adfcbfdf33d8cf095cc519
“Nader <nadmot23@gmail.com>
2024-04-12 16:33:34 +0000

    Finished queues-3

--- data-structures/queues-3.ts
+++ data-structures/queues-3.ts
@@ -13,15 +13,50 @@ export function takeValueAtIndex<T>(
     queue.enqueue(front);
     index--;
   }
-  return queue.peek();
+  return queue.dequeue();
 }
 
 export function takeSmaller(queue: Queue<number>): number | undefined {
-  return undefined;
+  if (queue.peek() !== undefined) {
+    const top = queue.dequeue();
+    if (queue.peek() !== undefined) {
+      const second = queue.dequeue();
+      if (top === undefined || second === undefined)
+        throw new Error('This will never happen');
+      if (top <= second) {
+        queue.enqueue(second);
+        return top;
+      } else {
+        queue.enqueue(top);
+        return second;
+      }
+    } else {
+      return top;
+    }
+  } else {
+    return undefined;
+  }
 }
 
 export function takeNextSmallest(
   numberQueue: Queue<number>
 ): number | undefined {
-  return undefined;
+  if (numberQueue.peek() !== undefined) {
+    let top = numberQueue.dequeue();
+    if (numberQueue.peek() !== undefined) {
+      let second = numberQueue.peek();
+      if (top === undefined || second === undefined)
+        throw new Error('This will never happen');
+      while (top !== undefined && second !== undefined && top > second) {
+        numberQueue.enqueue(top);
+        top = numberQueue.dequeue();
+        second = numberQueue.peek();
+      }
+      return top;
+    } else {
+      return top;
+    }
+  } else {
+    return undefined;
+  }
 }
